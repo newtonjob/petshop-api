35f1d24f8a8700e4297921ebd1878323e29d8fc0
newtonjob <jobnewton3@gmail.com>
2023-04-02 20:50:15 +0100

    Update the default migration for `users`

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->uuid();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->char('avatar', 36)->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->boolean('is_marketing')->default(false);
+            $table->timestamp('last_login_at')->nullable();
             $table->timestamps();
         });
     }
