9ab5244be69fb18b22a7eced3a7e143bb1597019
newtonjob <jobnewton3@gmail.com>
2023-04-02 21:23:40 +0100

    Create the migration and model for `Product`

---
+++ database/migrations/2023_04_02_201957_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignId('category_id')->constrained();
+            $table->string('title');
+            $table->float('price', 12);
+            $table->text('description')->nullable();
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
