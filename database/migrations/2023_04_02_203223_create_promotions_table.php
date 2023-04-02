bb2b3f094ed14fd598fa8d5c9c7204e22020ea26
newtonjob <jobnewton3@gmail.com>
2023-04-02 21:33:45 +0100

    Create the migration and model for `Promotion`

---
+++ database/migrations/2023_04_02_203223_create_promotions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('title');
+            $table->text('content');
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
