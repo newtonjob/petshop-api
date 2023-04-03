67cd8135e2e5042851af9d9c4244179635d9d944
newtonjob <jobnewton3@gmail.com>
2023-04-03 17:13:01 +0100

    Create migration for `jwt_tokens`

---
+++ database/migrations/2023_04_03_160942_create_jwt_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jwt_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->text('unique_id')->unique();
+            $table->string('token_title');
+            $table->json('restrictions')->nullable();
+            $table->json('permissions')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('refreshed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jwt_tokens');
+    }
+};
