89a1fe9386e6f59611d51f78e499ee0907e25f4c
newtonjob <jobnewton3@gmail.com>
2023-04-06 20:10:59 +0100

    Add php native return type

--- app/Models/Concerns/HasSlug.php
+++ app/Models/Concerns/HasSlug.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 
 trait HasSlug
 {
-    public static function bootHasSlug()
+    public static function bootHasSlug(): void
     {
         static::creating(function (Model $model) {
             $model->slug ??= Str::slug($model->title);
