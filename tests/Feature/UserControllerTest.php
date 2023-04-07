c4c13179c68508b6ed2537498565826cc6337452
newtonjob <jobnewton3@gmail.com>
2023-04-07 18:21:29 +0100

    Write tests for the UserControllerTest

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -12,11 +12,46 @@ class UserControllerTest extends TestCase
 
     public function test_admin_can_retrieved_users(): void
     {
-        $user = User::factory()->admin()->create();
+        $this->signInAsAdmin();
+
         User::factory(10)->create();
 
-        $response = $this->be($user, 'api')->get(route('admin.users.index'));
+        $response = $this->get(route('admin.users.index'));
 
         $this->assertNotEmpty($response['data']);
+    }
+
+    public function test_admin_can_update_users(): void
+    {
+        $this->signInAsAdmin();
+        $user = User::factory()->create();
+
+        $this->put(route('admin.users.update', $user), [
+            'first_name' => $name = fake()->firstName()
+        ])->assertOk();
+
+        $user->refresh();
+
+        $this->assertSame($user->first_name, $name);
+    }
+
+    public function test_admin_cannot_update_an_admin_account(): void
+    {
+        $this->signInAsAdmin();
+        $user = User::factory()->admin()->create();
+
+        $this->put(route('admin.users.update', $user), [
+            'first_name' => fake()->firstName()
+        ])->assertForbidden();
+    }
+
+    public function test_admin_can_delete_users(): void
+    {
+        $this->signInAsAdmin();
+        $user = User::factory()->create();
+
+        $this->delete(route('admin.users.destroy', $user))->assertOk();
+
+        $this->assertSoftDeleted($user);
     }
 }
