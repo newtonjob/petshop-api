caa326e36eb89c151fbeac7a6012d93e48a0edf3
newtonjob <jobnewton3@gmail.com>
2023-04-07 18:46:10 +0100

    Add test helper methods for sign in

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signInAsAdmin()
+    {
+        $this->be(User::factory()->admin()->create(), 'api');
+    }
+
+    public function signIn()
+    {
+        $this->be(User::factory()->create(), 'api');
+    }
 }
