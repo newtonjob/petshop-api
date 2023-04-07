73e403d1695cc8939e43431ef969db71c41e4550
newtonjob <jobnewton3@gmail.com>
2023-04-07 17:29:49 +0100

    Write tests for the AdminAuthController

---
+++ tests/Feature/AdminAuthControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AdminAuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_login(): void
+    {
+        $user = User::factory()->admin()->create([
+            'email' => 'jobnewton3@gmail.com'
+        ]);
+
+        $response = $this->post(route('admin.login'), [
+            'email'    => $user->email,
+            'password' => 'admin'
+        ]);
+
+        $response->assertOk();
+        $this->assertNotEmpty($response['data']['token']);
+    }
+
+    public function test_admin_token_can_be_used_to_access_authenticated_routes(): void
+    {
+        $user  = User::factory()->admin()->create();
+        $token = $user->createToken();
+
+        $this->withToken($token)->get(route('admin.users.index'))->assertOk();
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_admin_can_logout(): void
+    {
+        $user = User::factory()->admin()->create();
+
+        $user->createToken();
+
+        $this->actingAs($user, 'api')->post(route('admin.logout'))->assertOk();
+
+        $this->assertTrue($user->tokens()->doesntExist());
+    }
+}
